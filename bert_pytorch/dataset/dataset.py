27b6d82afbe65350c19070c63159630b8bfed74f
Meghan Lele <meghanl@fb.com>
2020-07-01 12:39:43 -0700

    [1] Installation and dataset

--- bert_pytorch/dataset/dataset.py
+++ bert_pytorch/dataset/dataset.py
@@ -20,7 +20,7 @@ class BERTDataset(Dataset):
                     self.corpus_lines += 1
 
             if on_memory:
-                self.lines = [line[:-1].split("\t")
+                self.lines = [line[:-1].split("\\t")
                               for line in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines)]
                 self.corpus_lines = len(self.lines)
 
